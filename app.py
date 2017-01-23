48770c9904be94dee64159b0a021e99964b34beb
Severin Schols <severin@schols.de>
2017-01-23 01:13:36 +0100

    basic UI

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from flask_cors import cross_origin
 from datetime import datetime, timedelta
 from peewee import *
@@ -34,7 +34,7 @@ class Device(BaseModel):
 		hourly_query = CounterEntry.select(fn.Sum(CounterEntry.duration).alias('total'), fn.strftime("%H", CounterEntry.timestamp).alias('hour')).where(CounterEntry.device == self).group_by(SQL('hour'))
 		hourly = {}
 		for entry in hourly_query:
-			hourly[entry.hour] = entry.total
+			hourly[int(entry.hour)] = entry.total
 
 		dow_query = CounterEntry.select(fn.Sum(CounterEntry.duration).alias('total'), fn.strftime("%w", CounterEntry.timestamp).alias('dow')).where(CounterEntry.device == self).group_by(SQL('dow'))
 		dow=  {}
@@ -43,8 +43,25 @@ class Device(BaseModel):
 
 		return { "hour": hourly, "dow": dow }
 
+	def getDaily(self):
+		daily_query = CounterEntry.select(fn.Sum(CounterEntry.duration).alias('total'), fn.strftime("%Y-%m-%d", CounterEntry.timestamp).alias('day')).where(CounterEntry.device == self).group_by(SQL('day'))
+		daily = {}
+		for entry in daily_query:
+			daily[entry.day] = entry.total
+
+		return daily
+
+	def getJobs(self,page=1, limit=100):
+		jobs_raw = self.counterentries.order_by(CounterEntry.timestamp.desc()).paginate(page,limit)
+
+		jobs=[]
+
+		for job in jobs_raw:
+			jobs.append({"duration": job.duration, "timestamp": job.timestamp})
+		return jobs
+
 class CounterEntry(BaseModel):
-	device = ForeignKeyField(Device)
+	device = ForeignKeyField(Device, related_name='counterentries')
 	duration = IntegerField()
 	timestamp = DateTimeField(default=datetime.now)
 
@@ -109,3 +126,39 @@ def getDeviceStats(name):
 	data = device.getStats()
 
 	return jsonify(data)
+
+@app.route("/api/device/<string:name>/jobs", methods=['GET'])
+@app.route("/api/device/<string:name>/jobs/<int:page>", methods=['GET'])
+@cross_origin()
+def getDeviceJobs(name, page=1):
+	try:
+		device = Device.get(name=name)
+	except:
+		return "not found"
+
+	return jsonify(device.getJobs(page))
+
+@app.route("/api/device/<string:name>/daily", methods=['GET'])
+@cross_origin()
+def getDeviceDaily(name):
+	try:
+		device = Device.get(name=name)
+	except:
+		return "not found"
+
+	data = device.getDaily()
+
+	return jsonify(data)
+
+@app.route("/", methods=['GET'])
+def uiIndex():
+	devices = Device.select();
+	return render_template('index.html', devices=devices)
+
+@app.route("/device/<string:name>", methods=['GET'])
+def uiDevice(name):
+	try:
+		device = Device.get(name=name)
+	except:
+		return "not found"
+	return render_template('device.html', device=device)
